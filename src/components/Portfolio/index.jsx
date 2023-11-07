975e1f759e5c97f8c0ca2f03e7adec86a7b6d167
Agustin Zamar <agustinzamar33@gmail.com>
2023-11-07 03:20:43 -0300

    improve portfolio images

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -3,6 +3,9 @@ import Heart from "../../icons/heart";
 import FunArrow from "../../icons/fun-arrow.jsx";
 
 import camcomexjujuy from "../../assets/camcomexjujuy.png";
+import expojuy from "../../assets/expojuy.png";
+import capse from "../../assets/capse.png";
+import espaciopurmamarca from "../../assets/espaciopurmamarca.png";
 
 export const Portfolio = () => {
   return (
@@ -18,24 +21,27 @@ export const Portfolio = () => {
         <FunArrow className="rotate-180 absolute left-[-20%] bottom-[-80%] flex flex-row items-center gap-2" />
       </div>
 
-      <div className="relative">
-        <div className="w-full flex flex-row overflow-x-scroll">
-          <img
-            src={camcomexjujuy.src}
-            alt="Sitio web de la Cámara de comercio exterior de Jujuy"
-            className="w-[800px]"
-          />
-          <img
-            src={camcomexjujuy.src}
-            alt="Sitio web de la Cámara de comercio exterior de Jujuy"
-            className="w-[800px]"
-          />
-          <img
-            src={camcomexjujuy.src}
-            alt="Sitio web de la Cámara de comercio exterior de Jujuy"
-            className="w-[800px]"
-          />
-        </div>
+      <div className="w-full flex flex-row overflow-x-scroll">
+        <img
+          src={camcomexjujuy.src}
+          alt="Sitio web de la Cámara de comercio exterior de Jujuy"
+          className="max-w-6xl"
+        />
+        <img
+          src={espaciopurmamarca.src}
+          alt="Sitio web de la Cámara de comercio exterior de Jujuy"
+          className="max-w-6xl"
+        />
+        <img
+          src={capse.src}
+          alt="Sitio web de la Cámara de comercio exterior de Jujuy"
+          className="max-w-4xl mr-48"
+        />
+        <img
+          src={expojuy.src}
+          alt="Sitio web de la Cámara de comercio exterior de Jujuy"
+          className="max-w-6xl"
+        />
       </div>
     </Section>
   );
