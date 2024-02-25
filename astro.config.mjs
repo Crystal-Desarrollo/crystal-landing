681617b6c0076953117f9e1f9ae0465abd38080b
Agustin Zamar <agustinzamar33@gmail.com>
2024-02-25 20:59:04 -0300

    remove fiolders .vscode and .idea

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,15 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://crystal-desarrollo.com',
+  site: "https://crystal-desarrollo.com",
   devOptions: {
-    tailwindConfig: './tailwind.config.js',
-  }
+    tailwindConfig: "./tailwind.config.js",
+  },
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
