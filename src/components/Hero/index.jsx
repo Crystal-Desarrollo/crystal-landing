0ba533dc9f76eb14aeaa8db6415300780234093e
Agustin Zamar <agustinzamar33@gmail.com>
2024-02-25 22:39:30 -0300

    add Hero translations

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,15 +1,14 @@
 import { Section } from "../Section";
 import bolt from "../../assets/bolt.svg";
+import { t } from "i18next";
 
 export const Hero = () => {
   return (
     <Section className="relative">
       <div className="flex flex-col items-center max-w-prose gap-2">
-        <h2 className="text-font-light text-center text-xl">
-          Transformamos tus ideas en
-        </h2>
+        <h2 className="text-font-light text-center text-xl">{t("home.hero.preTitle")}</h2>
         <h3 className="text-5xl text-center font-bold leading-snug">
-          Experiencias tecnológicas de máxima calidad
+          {t("home.hero.title")}
           <img
             loading="eager"
             width="30"
@@ -21,9 +20,7 @@ export const Hero = () => {
             alt="icono de rayo"
           ></img>
         </h3>
-        <h4 className="text-font-light text-center text-md">
-          Rápido, fácil y a medida para vos. ¡Somos tu solución digital!
-        </h4>
+        <h4 className="text-font-light text-center text-md">{t("home.hero.subtitle")}</h4>
       </div>
       <div className="flex flex-row gap-4 items-center justify-center">
         <a
@@ -31,13 +28,10 @@ export const Hero = () => {
           href="https://cal.com/crystal-desarrollo/contanos-tu-idea"
           target="_blank"
         >
-          Empecemos
+          {t("home.hero.cta")}
         </a>
-        <a
-          className="px-4 py-2 border-2 border-primary rounded-md font-semibold text-xl"
-          href="/#projects"
-        >
-          Proyectos
+        <a className="px-4 py-2 border-2 border-primary rounded-md font-semibold text-xl" href="/#projects">
+          {t("home.hero.cta2")}
         </a>
       </div>
     </Section>
