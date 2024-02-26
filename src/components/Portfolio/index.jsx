ef6e82b5d5f72b26ae5076676aab7bbb98e040c3
Agustin Zamar <agustinzamar33@gmail.com>
2024-02-26 11:44:17 -0300

    add portfolio zoom in animation

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -12,48 +12,48 @@ import aapdpp from "../../assets/projects/aapdpp.webp";
 export const Portfolio = () => {
   return (
     <Section id="projects">
-      <div className="flex flex-col items-center gap-2 mb-12 relative">
+      <div className="flex flex-col items-center gap-2 mb-12 relative ">
         <p className="text-xl text-font-light flex flex-row items-center">
           {t("ourWork.preTitle")}
           <Heart className="ml-1 h-8" />
         </p>
         <p className="text-5xl font-bold text-center relative">{t("ourWork.title")}</p>
-        <FunArrow className="rotate-180 absolute md:left-[-20%] bottom-[-80%] flex flex-row items-center gap-2" />
+        <FunArrow className="rotate-[150deg] absolute md:left-[40%] bottom-[-80%] flex flex-row items-center gap-2" />
       </div>
 
-      <div className="w-full flex flex-row items-center overflow-x-scroll gap-32 mt-8 px-8 scrollbar snap-x">
+      <div className="gap-32 mt-8 px-8 grid grid-cols-1 lg:grid-cols-2">
         <img
           src={camcomexjujuy.src}
           alt="Sitio web de la Cámara de comercio exterior de Jujuy"
-          className="snap-center max-w-lg md:max-w-2xl"
+          className="transition-transform duration-200 ease-in hover:scale-110"
           width={672}
           height={446}
         />
         <img
           src={espaciopurmamarca.src}
           alt="Sitio web de Espacio Purmamarca"
-          className="snap-center max-w-lg md:max-w-2xl"
+          className="transition-transform duration-200 ease-in hover:scale-110"
           width={672}
           height={446}
         />
         <img
           src={expojuy.src}
           alt="Sitio web de la ExpoJuy Edición 2022"
-          className="snap-center max-w-lg md:max-w-2xl"
+          className="transition-transform duration-200 ease-in hover:scale-110"
           width={672}
           height={446}
         />
         <img
           src={aapdpp.src}
           alt="Sitio web de la Asociación Argentina de Profesionales de Derecho Procesal Penal"
-          className="snap-center max-w-lg md:max-w-2xl"
+          className="transition-transform duration-200 ease-in hover:scale-110"
           width={672}
           height={446}
         />
         <img
           src={capse.src}
           alt="Sitio web de CAPSE"
-          className="snap-center max-w-lg md:max-w-2xl"
+          className=" transition-transform duration-200 ease-in hover:scale-110 col-span-1 lg:col-span-2 mx-auto"
           width={672}
           height={446}
         />
