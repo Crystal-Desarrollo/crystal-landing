03722a3583367a4b539bfd79202bfc4a7b95bb8d
Agustin Zamar <agustinzamar33@gmail.com>
2024-02-25 22:46:23 -0300

    add steps translations

--- src/components/Steps/index.jsx
+++ src/components/Steps/index.jsx
@@ -1,6 +1,7 @@
 import { Section } from "../Section";
 import ArrowRight from "../../icons/arrow-right.jsx";
 import Underline from "../../icons/underline.jsx";
+import { t } from "i18next";
 
 import one from "../../assets/1.svg";
 import two from "../../assets/2.svg";
@@ -11,35 +12,23 @@ export const Steps = () => {
     <Section>
       <div className="bg-gradient-to-b from-gray-700 via-gray-900 to-black p-8 rounded-2xl">
         <div className="flex flex-col items-center gap-2 mb-12">
-          <p className="text-xl text-font-light">Nuestro trabajo</p>
+          <p className="text-xl text-font-light">{t("steps.subtitle")}</p>
           <p className="text-5xl font-bold text-center relative">
-            En 3 simples pasos
+            {t("steps.title")}
             <Underline className="absolute sm:bottom-[-100%] left-[33%]" />
           </p>
         </div>
         <ul className="flex gap-16 flex-col lg:flex-row items-center justify-center">
           <li className="mb-8 relative">
-            <Step
-              image={one.src}
-              title="Diseño"
-              description="Escuchamos tus necesidades y buscamos la mejor forma de ayudarte usando la tecnología."
-            />
+            <Step image={one.src} title={t("steps.step1.title")} description={t("steps.step1.description")} />
             <ArrowRight className="absolute text-white rotate-90 top-[160%] right-[35%] lg:rotate-0 lg:top-[40%] lg:right-[-10%]" />
           </li>
           <li className="mb-8 relative">
-            <Step
-              image={two.src}
-              title="Desarrollo"
-              description="Desarrollamos tu proyecto y te mostramos los avances para que puedas aportar tu visión."
-            />
+            <Step image={two.src} title={t("steps.step2.title")} description={t("steps.step2.description")} />
             <ArrowRight className="absolute text-white rotate-90 top-[160%] right-[35%] lg:rotate-0 lg:top-[40%] lg:right-[-10%]" />
           </li>
           <li className="mb-8">
-            <Step
-              image={three.src}
-              title="Entrega"
-              description="Te brindamos manuales de uso y capacitamos a todo tu equipo en el uso del sistema."
-            />
+            <Step image={three.src} title={t("steps.step3.title")} description={t("steps.step3.description")} />
           </li>
         </ul>
       </div>
