b79e7dcc299e44a56be77d28ba9701e505c2351a
Agustin Zamar <agustinzamar33@gmail.com>
2024-02-26 01:42:55 -0300

    add faqs and contact us translations

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,3 +1,5 @@
+import { t } from "i18next";
+
 import { Section } from "../Section";
 import { Coffee } from "../../icons/coffee";
 
@@ -6,21 +8,16 @@ export const ContactUs = () => {
     <Section>
       <div className="flex flex-col items-center max-w-prose gap-2">
         <h3 className="text-5xl text-center font-bold leading-snug flex flex-row items-center">
-          Tomemos un café
+          {t("contact.title")}
           <Coffee className="ml-4" />
         </h3>
-        <h2 className="text-font-light text-center text-xl">
-          y empecemos a trabajar en tu proyecto
-        </h2>
+        <h2 className="text-font-light text-center text-xl">{t("contact.subtitle")}</h2>
       </div>
 
       <div className="w-full lg:w-[60vw] mt-8">
-        <a
-          href="https://cal.com/crystal-desarrollo/contanos-tu-idea"
-          target="_blank"
-        >
+        <a href="https://cal.com/crystal-desarrollo/contanos-tu-idea" target="_blank">
           <button className="rounded-xl border p-4 h-[4rem] md:h-[7rem] lg:px-16 text-2xl font-bold text-white cta-btn cursor-pointer hover:scale-105 transition duration-300 ease-out w-full hover:bg-primary hover:text-white">
-            Empecemos hoy mismo
+            {t("contact.cta")}
           </button>
         </a>
       </div>
--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -1,48 +1,30 @@
 import { useState } from "react";
 import chat from "../../assets/chat.svg";
 
-export const FAQ = () => {
+export const FAQ = ({ title, q1, a1, q2, a2, q3, a3, q4, a4, asideTitle, asideSubtitle, asideCta }) => {
   return (
     <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center my-8">
       <div className="w-full">
         <div className="w-full flex flex-col items-center gap-6 mb-12 relative">
-          <p className="text-5xl font-bold text-center relative text-white">
-            Las respuestas que buscás
-          </p>
+          <p className="text-5xl font-bold text-pretty text-center relative text-white">{title}</p>
         </div>
         <div className="w-full flex flex-col">
-          <Collapse
-            question="¿Cómo trabajamos juntos en el proceso de desarrollo?"
-            answer="Nuestro proceso incluye consultas iniciales para comprender tus necesidades, diseño y desarrollo iterativo con tu retroalimentación constante, pruebas exhaustivas y finalmente, la implementación en tu servidor o alojamiento web."
-          />
-          <Collapse
-            question="¿Pueden mantener y actualizar mi sitio web después de la entrega?"
-            answer="Sí, ofrecemos servicios de mantenimiento y actualización de sitios web para garantizar que tu sitio esté siempre funcionando de manera óptima y actualizado con las últimas tecnologías."
-          />
-          <Collapse
-            question="¿Pueden ayudarme a migrar mi sitio web actual a una nueva plataforma? "
-            answer="Sí, ofrecemos servicios de migración de sitios web, lo que te permite cambiar a una plataforma más moderna y personalizada sin perder datos ni funcionalidades."
-          />
-          <Collapse
-            question="¿Cómo gestionan los problemas técnicos o las interrupciones en el sitio web después del lanzamiento?"
-            answer="Ofrecemos soporte técnico continuo y solucionamos problemas rápidamente. También implementamos monitoreo constante para prevenir problemas antes de que ocurran."
-          />
+          <Collapse question={q1} answer={a1} />
+          <Collapse question={q2} answer={a2} />
+          <Collapse question={q3} answer={a3} />
+          <Collapse question={q4} answer={a4} />
         </div>
       </div>
       <div className="flex flex-col items-center gap-6">
         <img alt="icono de mensaje" src={chat.src} />
 
-        <p className="text-3xl font-bold text-center relative text-white">
-          Te quedó alguna duda?
-        </p>
-        <p className="text-xl text-font-light text-center">
-          Estamos ansiosos por responderlas
-        </p>
+        <p className="text-3xl font-bold text-center relative text-white">{asideTitle}</p>
+        <p className="text-xl text-font-light text-center">{asideSubtitle}</p>
         <a
           href="https://cal.com/crystal-desarrollo/contanos-tu-idea"
           className="bg-primary text-white px-8 py-4 flex items-center rounded-md font-semibold text-lg hover:scale-105 ease-in-out duration-300"
         >
-          ¡Agenda tu reunión!
+          {asideCta}
         </a>
       </div>
     </div>
@@ -54,15 +36,10 @@ export const Collapse = ({ question, answer }) => {
 
   return (
     <div className="item px-6 py-6 w-full">
-      <button
-        onClick={() => setOpen(!open)}
-        className="w-full flex flex-row justify-between"
-      >
+      <button onClick={() => setOpen(!open)} className="w-full flex flex-row justify-between">
         <h4 className="text-font-light font-medium text-start">{question}</h4>
         <svg
-          className={`transform w-5 h-5 text-gray-500 ${
-            open ? "rotate-180" : ""
-          }`}
+          className={`transform w-5 h-5 text-gray-500 ${open ? "rotate-180" : ""}`}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
