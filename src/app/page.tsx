98ed9d35ee5fdf226af1e0a1266a6d48aac344cc
Agustin Zamar <agustinzamar33@gmail.com>
2025-08-26 17:17:16 -0300

    add tech stack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import {HeroSection} from "@/partials/home/hero-section";
 import {Projects} from "@/partials/home/projects";
 import {Members} from "@/partials/home/members";
 import React from "react";
+import {TechStack} from "@/partials/home/tech-stack";
 
 export default function Home() {
     return (
         <div>
             <HeroSection/>
+            <TechStack/>
 
             <Projects/>
-
             <Members/>
         </div>
     );
