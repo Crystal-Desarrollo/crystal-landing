2c14617a41ae72674a4a205d27f6c76e81d2940a
Agustin Zamar <agustinzamar33@gmail.com>
2025-09-18 00:51:53 -0300

    update CTA section layout and improve text styling for enhanced visibility

--- resources/views/components/home/cta.blade.php
+++ resources/views/components/home/cta.blade.php
@@ -1,17 +1,17 @@
 <header
-  class="w-full rounded-xl bg-black px-8 py-12 flex flex-col md:flex-row items-center justify-between container mx-auto gap-8"
+  class="w-full rounded-xl bg-black  p-16 flex flex-col md:flex-row items-center justify-between container mx-auto gap-24"
 >
     <div class="flex flex-col items-center md:items-start gap-2">
-        <h3 class="text-white font-bold text-xl xl:text-2xl text-center md:text-left md:text-pretty">
+        <h4 class="text-white font-bold text-xl lg:text-4xl text-center md:text-left md:text-pretty">
             Acelera tu camino al éxito
-        </h3>
-        <p class="text-gray-400 text-base xl:text-xl text-center md:text-pretty md:text-left">
+        </h4>
+        <p class="text-gray-400 text-base lg:text-2xl text-center md:text-pretty md:text-left">
             Trabajemos juntos para convertir tus ideas en realidad.
         </p>
     </div>
     
     <button
-      class="cursor-pointer border border-slate-600 px-6 py-2 rounded-full text-slate-400 hover:bg-slate-900 text-xl xl:text-2xl"
+      class="cursor-pointer border border-slate-600 px-6 py-4 rounded-full text-slate-400 hover:bg-slate-800 text-xl lg:text-2xl"
     >
         Contactanos
     </button>
