350de221214ec24245b51176b664fedfd7a4fda3
Agustin Zamar <agustinzamar33@gmail.com>
2025-11-03 16:56:26 -0300

    fix: update project title and button text for localization

--- resources/views/components/home/projects.blade.php
+++ resources/views/components/home/projects.blade.php
@@ -1,6 +1,6 @@
 <div class="container mx-auto flex flex-col items-center mt-6 lg:mt-16">
-    <x-ui.title badge-text="Clientes">
-        Conocé los <span class="text-accent-content">casos de éxito</span> de quienes ya confian en nosotros.
+    <x-ui.title badge-text="{{ __('pages/home.projects.badge') }}">
+        {!! __('pages/home.projects.title') !!}
     </x-ui.title>
     
     <x-projects.projects-list :projects="$projects"/>
@@ -10,6 +10,6 @@
       rel="noopener noreferrer"
       class="px-4 py-2 mt-6 w-full lg:max-w-40 justify-center text-center lg:text-xl rounded-full bg-accent hover:bg-accent/90 text-white inline-flex items-center gap-2"
     >
-        Ver todos
+        {{ __('pages/home.projects.view_all') }}
     </a>
 </div>
