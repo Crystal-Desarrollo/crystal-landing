6532acf5a19362e68f91b3d12afb52c9cd8c8c53
Agustin Zamar <agustinzamar33@gmail.com>
2025-11-03 16:53:41 -0300

    fix: update project card class for styling consistency

--- resources/views/components/projects/project-card.blade.php
+++ resources/views/components/projects/project-card.blade.php
@@ -7,7 +7,7 @@
 
 <a
   href="{{route($routeName)}}"
-  class="relative overflow-hidden group rounded-2xl drop-shadow-2xl w-full max-w-md h-80 lg:min-h-80 p-8 flex flex-col justify-start lg:flex-row gap-4 lg:gap-6 {{$class}}"
+  class="project-card relative overflow-hidden group rounded-2xl drop-shadow-2xl w-full max-w-md h-80 lg:min-h-80 p-8 flex flex-col justify-start lg:flex-row gap-4 lg:gap-6 {{$class}}"
 >
     <div class="flex justify-between h-min w-full gap-2">
         <h3 class="font-semibold text-xl lg:text-2xl leading-relaxed tracking-wider">{!! $title !!}</h3>
